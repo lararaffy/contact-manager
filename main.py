c95816b0111065d81c3719903b96017e29893f89
Kailany7 <Kailanysilva118@gmail.com>
2024-06-09 18:32:30 -0300

    Menu principal

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 import contatos
-print (contatos.add_contato)
+def menu():
+    while True:
+        print("===== BEM VINDO AO GERENCIAMENTO DE CONTATOS =====")
+        print("1 - Adicionar contato")
+        print("2 - Remover contato")
+        print("3 - Buscar contato")
+        print("4 - Lista de contato")
+        print("5 - SAIR")
+        opcao = int(input("Digite a opção desejada: "))
+
+        if opcao == 1:
+            contatos.add_contato()
+        elif opcao == 2:
+            contatos.remove_contato()
+        elif opcao == 3:
+            contatos.procurar_contato()
+        elif opcao == 4:
+            contatos.lista_contatos()
+        elif opcao == 5:
+            contatos.sair()
+        else:
+         print("Opção inválida. TENTE NOVAMENTE!")
+
+
+if _name_ == "_main_":
+    menu()
